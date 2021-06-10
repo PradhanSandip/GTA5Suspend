bb9ae954db9fa8dd9d44db2cdc9af4352a3368cf
MrDuck6 <mrduckandduck6@gmail.com>
2021-06-10 12:25:29 +0100

    Update suspend_and_resume.bat

--- suspend_and_resume.bat
+++ suspend_and_resume.bat
@@ -2,12 +2,11 @@
 tasklist /fi "imagename eq GTA5.exe" | find /i "GTA5.exe" > nul
 if not errorlevel 1 (NUL) else (
     echo "GTA NOT RUNNING"
-    PAUSE
-    EXIT
+    pause
+    exit
 )
 
 pssuspend.exe GTA5.exe
-@ECHO off
 @echo "GTA5.exe process suspended and will resume in 10 seconds"
 timeout 10 >nul /nobreak
 pssuspend.exe -r GTA5.exe
